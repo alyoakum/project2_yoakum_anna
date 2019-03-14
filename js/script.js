e0f430a6cd2e6aa31638f05c3b29837a1bac3db5
Anna Yoakum <annayoakum@Annas-MacBook-Pro.local>
2019-03-14 03:06:58 -0500

    Added lazy load plugin to illustration gallery page.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ $('.smooth').on('click', function() {
 
     return false;
 });
+
+
+
+window.addEventListener('load', function(){
+    var allimages= document.getElementsByTagName('img');
+    for (var i=0; i<allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false)
