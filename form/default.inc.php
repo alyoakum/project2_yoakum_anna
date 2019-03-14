b4933924ee5ed3262c00a8525d386eba705783e1
Anna Yoakum <annayoakum@Annas-MacBook-Pro.local>
2019-03-13 21:29:58 -0500

    Initial Commit

---
+++ form/default.inc.php
@@ -0,0 +1,10 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</li>
+    </ul>
+  </nav>
+</header>
+    <div class="container">
+  <p>Sorry, looks like something went wrong. Please try again. The fate of the Seven Kingdoms depends on you.
+  </div>
